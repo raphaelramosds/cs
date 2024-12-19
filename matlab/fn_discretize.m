d5b2b0c6bd600fd049dea1a31af13189d31fc6cd
Raphael Ramos <raphael.opensource@gmail.com>
2024-12-19 16:02:29 -0300

    discretize continuous system ok

---
+++ matlab/fn_discretize.m
@@ -0,0 +1,20 @@
+function [G, H] = fn_discretize(A,B, T)
+  % Discretiza um sistema discreto (A,B,C,D) com periodo de amostragem T segundos
+  % @param A, B
+  % @param T
+  % @returns G, H
+
+  % Literais
+  syms s t;
+
+  % Ordem do sistema
+  n = size(A,1);
+
+  % Calculo de e^(AT) e integral de e^(At)*B, com t=0..T
+  eAt = ilaplace(inv(s*eye(n,n)-A),s,t);
+  eAtB = eAt * B;
+
+  % Calculo das matrizes G e H
+  G = double(subs(eAt, t, T))
+  H = double(int(eAtB, t, 0, T))
+end
--- matlab/main.m
+++ matlab/main.m
@@ -4,14 +4,7 @@ syms s;
 
 clc;
 
-A = [-4 3 0; -1 -4 -1; 0 0 -2];
-B = [0; 0; 3];
-C = [1 0 0];
-polos = [-2 -2 -3];
+A = [0 1; 0 -2];
+B = [0; 1];
 
-N = 0*s+3;
-D = s^2+2*s-4;
-
-[Aco, Bco, Cco, Dco] = fn_robsv(N,D);
-
-fn_segreferencia(Aco,Bco,Cco,polos);
+fn_discretize(A,B, 1)
